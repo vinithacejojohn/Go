7017fd153e4234073ed7d0eef4da66631f9101ff
johnv1 <Vinithamol.Balachandran@cba.com.au>
2022-09-26 19:51:09 +1000

    newDeck func

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Cards"
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
+	cards := newDeck()
 
-func newCard() string {
-
-	return "Ace of Cards"
+	cards.print()
 }
